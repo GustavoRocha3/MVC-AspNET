cdf3380934d5a5f2ff31ac53105bcfbf737d3345
GustavoRocha3 <gsutavo.barros3@outlook.com>
2022-05-28 18:26:25 -0300

    meu primeiro site em Asp.Net

---
+++ MVC-Website/MVC-Website/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Website.Models;
+using System.Diagnostics;
+
+namespace MVC_Website.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+
+            home.Nome = "Gustavo de Alencar";
+            home.Email = "gustavo.barros3@outlook.com";
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC-Website/MVC-Website/Models/HomeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_Website.Models
+{
+    public class HomeModel
+    {
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+    }
+}
